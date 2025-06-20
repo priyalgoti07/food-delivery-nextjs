907f826e7e011a3dd9692aea9f9026119ab3aeb8
priyalgoti07 <gotipriyal1996@gmail.com>
2025-06-20 13:35:26 +0530

    cart data store localstorage through persist

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,10 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { persistReducer, persistStore } from "redux-persist";
 
-export const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    }
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']   // only persist cart slice
+}
+
+const rootReducer = combineReducers({
+    cart: cartReducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false
+        })
+})
+export const persistor = persistStore(store)
 export default store;
