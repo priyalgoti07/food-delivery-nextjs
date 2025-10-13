ee59ea592e31897aa86ad74cbff76959666d5284
priyalgoti07 <gotipriyal1996@gmail.com>
2025-10-13 16:25:42 +0530

    mui drawer add in signup onclick open drawer

--- src/app/_components/HomePageHeader.js
+++ src/app/_components/HomePageHeader.js
@@ -2,15 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import logo from "../../../public/food-logo.png";
-import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
+import { SwipeableDrawer } from "@mui/material";
 
 const HomePageHeader = () => {
     const router = useRouter();
     const [popup, setPopup] = useState(false);
     const userStorage = JSON.parse(localStorage.getItem('user'));
     const [user] = useState(userStorage ? userStorage : undefined);
-
+    const [openDrawer, setOpenDrawer] = useState(false);
     const handleLogout = () => {
         localStorage.removeItem("user")
         router.push("/user-auth")
@@ -26,52 +26,71 @@ const HomePageHeader = () => {
     }
 
     return (
-        <header className="w-full py-4 px-6 z-11 bg-orange-400 text-white">
-            <div className="max-w-7xl mx-auto flex justify-between items-center">
-                {/* Logo */}
-                <Link href={'/'} className="flex items-center space-x-2 cursor-pointer">
-                    <Image src={logo} alt="Restaurant Logo" width={60} height={60} />
-                </Link>
+        <>
+            <header className="w-full py-4 px-6 z-11 bg-orange-400 text-white">
+                <div className="max-w-7xl mx-auto flex justify-between items-center">
+                    {/* Logo */}
+                    <Link href={'/'} className="flex items-center space-x-2 cursor-pointer">
+                        <Image src={logo} alt="Restaurant Logo" width={60} height={60} />
+                    </Link>
 
-                {/* Navigation Links */}
-                <nav className="flex justify-around">
-                    <ul className="flex space-x-6">
-                        <ListItem href='/deliverydashboard' title="Delivery Partner with us" />
-                        {
-                            user ?
-                                <>
-                                    <ListItem href='/myprofile' title={user?.name} />
+                    {/* Navigation Links */}
+                    <nav className="flex justify-around">
+                        <ul className="flex space-x-6 items-center">
+                            <ListItem href='/deliverydashboard' title="Delivery Partner with us" />
+                            {
+                                user ?
+                                    <>
+                                        <ListItem href='/myprofile' title={user?.name} />
+                                        <li>
+                                            <button onClick={() => setPopup(true)} className="font-bold leading-5 tracking-wider">Logout</button>
+                                        </li>
+                                    </> :
+                                    // <ListItem href='/user-auth' title='Login' />
                                     <li>
-                                        <button onClick={() => setPopup(true)} className="font-bold leading-5 tracking-wider">Logout</button>
+                                        <button onClick={() => setOpenDrawer(true)} className="font-bold leading-5 tracking-wider  py-3 px-6 bg-black rounded-xl">Sign in</button>
                                     </li>
-                                </> :
-                                <ListItem href='/user-auth' title='Login' />
-                        }
-                    </ul>
-                </nav>
-            </div>
-            {popup &&
-                <div className="fixed inset-0 flex items-center justify-center bg-white/30 backdrop-blur-sm z-50">
-                    <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-                        <h2 className="text-lg font-semibold mb-4">Are you sure you want to logout?</h2>
-                        <div className="flex justify-end gap-4">
-                            <button
-                                onClick={() => setPopup(false)}
-                                className="px-4 py-2 rounded border text-gray-600 hover:bg-gray-100"
-                            >
-                                Cancel
-                            </button>
-                            <button
-                                onClick={handleLogout}
-                                className="px-4 py-2 rounded bg-red-500 text-white hover:bg-red-600"
-                            >
-                                Yes, Logout
-                            </button>
+                            }
+                        </ul>
+                    </nav>
+                </div>
+                {popup &&
+                    <div className="fixed inset-0 flex items-center justify-center bg-white/30 backdrop-blur-sm z-50">
+                        <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
+                            <h2 className="text-lg font-semibold mb-4">Are you sure you want to logout?</h2>
+                            <div className="flex justify-end gap-4">
+                                <button
+                                    onClick={() => setPopup(false)}
+                                    className="px-4 py-2 rounded border text-gray-600 hover:bg-gray-100"
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    onClick={handleLogout}
+                                    className="px-4 py-2 rounded bg-red-500 text-white hover:bg-red-600"
+                                >
+                                    Yes, Logout
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                }
+            </header>
+            {
+                <SwipeableDrawer open={openDrawer} onClose={() => setOpenDrawer(false)} anchor="right"
+                    slotProps={{
+                        paper: {
+                            sx: {
+                                width: 700, // custom width
+                                p: 2,       // optional padding
+                                boxSizing: "border-box",
+                            },
+                        },
+                    }}>
+                    <h1>Sign up</h1>
+                </SwipeableDrawer>
             }
-        </header>
+        </>
     );
 };
 
